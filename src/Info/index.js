390c2717b8fa59920eebc592447410f1cadd5844
Joe Cillo <JCillo507@yahoo.com>
2020-02-05 17:19:43 -0500

    info done

--- src/Info/index.js
+++ src/Info/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './info.scss'
 
 const Info = ()=>{
   return(
     <div className='info-ctr'>
-      <p>
+      <p className='info-text'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Mauris pellentesque pulvinar pellentesque habitant morbi tristique. 
--- src/Services/index.js
+++ src/Services/index.js
@@ -4,6 +4,9 @@ import Card from '../Card'
 const Services = ()=>{
   return (
     <div className='services-ctr'>
+      <h2>
+        Services
+      </h2>
       <Card title='hello' desc='working'/>
       <Card title='hello1' desc='working1' />
       <Card title='hello2' desc='working2' />
