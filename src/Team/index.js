24f2c7ab3a971a247f45b53f2c363f416e9e3fc0
Joe Cillo <JCillo507@yahoo.com>
2020-02-06 15:59:16 -0500

    added team desc

--- src/Team/index.js
+++ src/Team/index.js
@@ -8,12 +8,18 @@ import P4 from '../assets/p4.png'
 
 const Team =()=>{
   return(
+    <div>
+      <h2 className='team-title'>
+        Meet the Team
+        </h2>
+      <p className='team-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     <div className='team-ctr'>
       <PCard name='Boss' title='CEO' email='CEO@company.com'image={P1}/>
       <PCard name='Boss1' title='CTO' email='CTO@company.com' image={P2}/>
       <PCard name='Boss2' title='COO' email='COO@company.com' image={P3}/>
       <PCard name='Boss3' title='CFO' email='CFO@company.com' image={P4}/>
     </div>
+    </div >
   )
 }
 
